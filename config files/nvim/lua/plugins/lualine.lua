a6d648af3ed7321497475f580c1209a23a76b9dd
Mehedi Hasan <mehediuc11@gmail.com>
2025-07-08 20:16:51 +0600

    Update lualine.lua

--- config files/nvim/lua/plugins/lualine.lua
+++ config files/nvim/lua/plugins/lualine.lua
@@ -1,12 +1,27 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				section_separators = { left = "", right = "" },
-				component_separators = { left = "", right = "" },
-			},
-		})
-	end,
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require("lualine").setup({
+      options = {
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = {
+          {
+            'mode',
+            fmt = function(str)
+              return ' ' .. str
+            end,
+          },
+        },
+        lualine_b = { 'branch' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+    })
+  end,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
 }
